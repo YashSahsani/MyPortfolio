3dc2ae875737a3ae9f180f5c04fa532f5e2b23d7
YashSahsani <yashsahsani5@gmail.com>
2020-08-31 09:35:38 +0530

    Updates

---
+++ precache-manifest.a4ae94245ba75e34a3c597e0a5e9f862.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "198d3917d4808beb43f1d7a54a360d52",
+    "url": "/YashSahsani/MyPortfolio/index.html"
+  },
+  {
+    "revision": "84634f17b9d0d0998209",
+    "url": "/YashSahsani/MyPortfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d599012c8fe8317baf64",
+    "url": "/YashSahsani/MyPortfolio/static/js/2.7fa06f07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/YashSahsani/MyPortfolio/static/js/2.7fa06f07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84634f17b9d0d0998209",
+    "url": "/YashSahsani/MyPortfolio/static/js/main.e12fac27.chunk.js"
+  },
+  {
+    "revision": "179236201b166f75e745",
+    "url": "/YashSahsani/MyPortfolio/static/js/runtime-main.473e7a3f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/YashSahsani/MyPortfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
